0465b184d5600da674f2c41ab020931be14b5e71
Cooper Riehl <metakazel@gmail.com>
2014-04-01 14:34:48 -0400

    Initial commit

---
+++ Snake/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Messaging;
+using System.Threading.Tasks;
+using Timer = System.Timers.Timer;
+
+namespace Snake
+{
+	static class Program
+	{
+		public const int Width = 80, Height = 24;
+		static private SnakeDirection _direction = SnakeDirection.Right;
+		private static bool _playing = true;
+		private const int GameSpeed = 40;
+
+		public enum SnakeDirection
+		{
+			Up,
+			Down,
+			Left,
+			Right,
+		};
+
+		public struct Food
+		{
+			public int x;
+			public int y;
+		};
+
+		static void Buffer(Snake snake, Food food, string[,] buffer)
+		{
+			// Buffer Borders
+			for (var row = 0; row < Height; row++) {
+				for (var col = 0; col < Width; col++) {
+					if (row == 0 || row == Height - 1 || col == 0 || col == Width - 1) {
+						buffer[col, row] = "#";
+					}
+				}
+			}
+
+			// Buffer Snake
+			snake.Draw(buffer);
+
+			// Buffer Food
+			buffer[food.x, food.y] = "$";
+		}
+
+		static void Clear(Snake snake, Food food, string[,] buffer)
+		{
+			// Clear Snake
+			snake.Clear(buffer);
+
+			// Clear Food
+			buffer[food.x, food.y] = " ";
+		}
+
+		static void InitializeBuffer(string[,] buffer)
+		{
+			// Fill with blank spaces
+			for (var row = 0; row < Height; row++) {
+				for (var col = 0; col < Width; col++) {
+					buffer[col, row] = " ";
+				}
+			}
+		}
+
+		static void GameLoop()
+		{
+			var rand = new Random();
+			var curBuffer = 0;
+			var buffers = new string[2][,];
+
+			for (var i = 0; i < 2; i++) {
+				buffers[i] = new string[Width, Height];
+				InitializeBuffer(buffers[i]);
+			}
+
+			var snake = new Snake(10, 10);
+			Food food;
+
+			food.x = rand.Next(Width - 2) + 1;
+			food.y = rand.Next(Height - 2) + 1;
+
+			// main game loop
+			while (_playing) {
+				Clear(snake, food, buffers[curBuffer]);
+				curBuffer = (curBuffer + 1) % 2;
+
+				snake.Move(_direction);
+
+				if (snake.DetectFood(food)) {
+					snake.AddSegment();
+					food.x = rand.Next(Width - 2) + 1;
+					food.y = rand.Next(Height - 2) + 1;
+				}
+
+				if (snake.DetectCollision()) {
+					_playing = false;
+					Console.Write("\bYou are a loser!");
+				} else {
+					Buffer(snake, food, buffers[curBuffer]);
+
+					// Write to console
+					for (var i = 0; i < Height; i++) {
+						Console.SetCursorPosition(0, i);
+						for (var j = 0; j < Width; j++) {
+							Console.Write(buffers[curBuffer][j, i]);
+						}
+					}
+				}
+				Thread.Sleep(GameSpeed);
+			}
+		}
+
+		static void GetUserInput()
+		{
+			while (true) {
+				var key = Console.ReadKey().Key;
+
+				switch (key) {
+					case ConsoleKey.UpArrow:
+						_direction = SnakeDirection.Up;
+						break;
+					case ConsoleKey.DownArrow:
+						_direction = SnakeDirection.Down;
+						break;
+					case ConsoleKey.LeftArrow:
+						_direction = SnakeDirection.Left;
+						break;
+					case ConsoleKey.RightArrow:
+						_direction = SnakeDirection.Right;
+						break;
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var gameLoop = new Thread(new ThreadStart(GameLoop));
+			var userInput = new Thread(new ThreadStart(GetUserInput));
+			gameLoop.Start();
+			userInput.Start();
+
+			while (_playing) {
+				userInput.Join(GameSpeed);
+				gameLoop.Join();
+			}
+			userInput.Abort();
+			gameLoop.Abort();
+		}
+	}
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Snake : IBufferable
+	{
+		private struct Segment
+		{
+			public int X;
+			public int Y;
+			public int Id;
+			public Program.SnakeDirection direction;
+		};
+		
+		private List<Segment> _segments =  new List<Segment>();
+
+		public Snake(int startX, int startY)
+		{
+			// Initialize the snake "head"
+			this._segments.Add(new Segment(){X = startX, Y = startY, Id = 1, direction = Program.SnakeDirection.Right});
+		}
+
+		// Adds a segment to the end of the snake
+		public void AddSegment()
+		{
+			var tail = _segments.Find(x => x.Id == _segments.Count);
+			var xnew = tail.X;
+			var ynew = tail.Y;
+			switch (tail.direction) {
+				case Program.SnakeDirection.Left:
+					xnew = tail.X + 1;
+					break;
+				case Program.SnakeDirection.Right:
+					xnew = tail.X - 1;
+					break;
+				case Program.SnakeDirection.Up:
+					ynew = tail.Y + 1;
+					break;
+				case Program.SnakeDirection.Down:
+					ynew = tail.Y - 1;
+					break;
+			}
+			_segments.Add(new Segment(){X = xnew, Y = ynew, direction = tail.direction, Id = tail.Id + 1});
+		}
+
+		// Moves the Snake one unit, with the new head going in the specified direction
+		public void Move(Program.SnakeDirection direction)
+		{
+			// Move the tail of the snake to the position of the head + 1
+			var head = _segments.Find(x => x.Id == 1);
+			var xnew = head.X;
+			var ynew = head.Y;
+			switch (direction) {
+				case Program.SnakeDirection.Left:
+					if (head.direction == Program.SnakeDirection.Right) {
+						xnew = head.X + 1;
+						direction = Program.SnakeDirection.Right;
+					} else
+						xnew = head.X - 1;
+					break;
+				case Program.SnakeDirection.Right:
+					if (head.direction == Program.SnakeDirection.Left) {
+						xnew = head.X - 1;
+						direction = Program.SnakeDirection.Left;
+					} else
+						xnew = head.X + 1;
+					break;
+				case Program.SnakeDirection.Up:
+					if (head.direction == Program.SnakeDirection.Down) {
+						ynew = head.Y + 1;
+						direction = Program.SnakeDirection.Down;
+					} else
+						ynew = head.Y - 1;
+					break;
+				case Program.SnakeDirection.Down:
+					if (head.direction == Program.SnakeDirection.Up) {
+						ynew = head.Y - 1;
+						direction = Program.SnakeDirection.Up;
+					} else
+						ynew = head.Y + 1;
+					break;
+			}
+
+			_segments.RemoveAt(_segments.FindIndex(x => x.Id == _segments.Count));
+			_segments.Add(new Segment(){X = xnew, Y = ynew, direction = direction, Id = 0});
+
+			// Increment the ID by one
+			// There has to be a better way to do this...
+			for (int i = 0; i < _segments.Count; i++) {
+				_segments[i] = new Segment() {
+					X = _segments[i].X,
+					Y = _segments[i].Y,
+					direction = _segments[i].direction,
+					Id = _segments[i].Id + 1
+				};
+			}
+		}
+
+		public bool DetectCollision()
+		{
+			var head = _segments.Find(x => x.Id == 1);
+			// First check to see if this snake is colliding with its own segments
+			if (this._segments.Any(seg => head.X == seg.X && head.Y == seg.Y && head.Id != seg.Id)) {
+				return true;
+			}
+			// Finally check to see if we are on a wall
+			return head.Y == 0 || head.Y == Program.Height - 1 || head.X == 0 ||
+			       head.X == Program.Width - 1;
+		}
+
+		public bool DetectFood(Program.Food food)
+		{
+			var head = _segments.Find(x => x.Id == 1);
+			return head.X == food.x && head.Y == food.y;
+		}
+
+		// Draws this Snake onto the specified buffer
+		public void Draw(string[,] buffer)
+		{
+			foreach (var seg in _segments) {
+				buffer[seg.X, seg.Y] = "@";
+			}
+		}
+
+		// Clear this Snake from the specified buffer
+		public void Clear(string[,] buffer)
+		{
+			foreach (var seg in _segments) {
+				buffer[seg.X, seg.Y] = " ";
+			}
+		}
+
+		public int GetNumSegments()
+		{
+			return _segments.Count;
+		}
+	}
+}
